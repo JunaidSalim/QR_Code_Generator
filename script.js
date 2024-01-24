2608c7bf7d1e67918d80cfacaa18c27b4e294dc8
Junaid Saleem <junaidsaleem986@gmail.com>
2024-01-24 22:54:51 +0500

    updated

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-function Generate()
-{
+function Generate() {
     var url = "https://chart.googleapis.com/chart?cht=qr&"
     var text = document.getElementById("text").value;
     var height = document.getElementById("height").value;
     var width = document.getElementById("width").value;
-    url = url + "chl=" + text + "&chs="+ width + "x" + height;
+    url = url + "chl=" + text + "&chs=" + width + "x" + height + "&chf=bg,s,00000000";
     document.getElementById("qr").src = url;
-
+    var qrcode = document.getElementById("qr");
+    qrcode.style.padding = "3px";
+    qrcode.style.border = "3px solid yellow";
 }
+
